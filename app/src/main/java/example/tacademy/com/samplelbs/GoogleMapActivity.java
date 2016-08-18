93a950b3bbead9c100f5fe74c43c8ab7606eeef6
KSJ <smart23033@gmail.com>
2016-08-18 17:42:48 +0900

    google marker

--- app/src/main/java/example/tacademy/com/samplelbs/GoogleMapActivity.java
+++ app/src/main/java/example/tacademy/com/samplelbs/GoogleMapActivity.java
@@ -6,9 +6,13 @@ import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Handler;
+import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -16,11 +20,18 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.Projection;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class GoogleMapActivity extends AppCompatActivity
-        implements OnMapReadyCallback, GoogleMap.OnCameraMoveListener {
+public class GoogleMapActivity extends AppCompatActivity implements
+        OnMapReadyCallback, GoogleMap.OnCameraMoveListener,
+        GoogleMap.OnMapClickListener,
+        GoogleMap.OnMarkerClickListener,
+        GoogleMap.OnInfoWindowClickListener,
+        GoogleMap.OnMarkerDragListener{
 
     GoogleMap map;
     LocationManager mLM;
@@ -53,7 +64,66 @@ public class GoogleMapActivity extends AppCompatActivity
         map.getUiSettings().setCompassEnabled(true);
         map.getUiSettings().setZoomControlsEnabled(true);
         map.setOnCameraMoveListener(this);
+        map.setOnMapClickListener(this);
+        map.setOnMarkerClickListener(this);
+        map.setOnInfoWindowClickListener(this);
+        map.setOnMarkerDragListener(this);
+    }
 
+    @Override
+    public void onMarkerDragStart(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDrag(Marker marker) {
+
+    }
+
+    @Override
+    public void onMarkerDragEnd(Marker marker) {
+        LatLng latLng = marker.getPosition();
+        Log.i("GoogleMapActivity","lat : " + latLng.latitude + ", lng : " + latLng.longitude);
+    }
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+        marker.hideInfoWindow();
+    }
+
+    Handler mHandler = new Handler(Looper.getMainLooper());
+    @Override
+    public boolean onMarkerClick(final Marker marker) {
+        Toast.makeText(this,marker.getTitle(),Toast.LENGTH_SHORT).show();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                marker.showInfoWindow();
+            }
+        },2000);
+        return true;
+    }
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+        addMarker(latLng.latitude,latLng.longitude, "My marker");
+    }
+
+    Marker marker;
+    private void addMarker(double lat, double lng, String title){
+        if(marker != null){
+            marker.remove();
+            marker = null;
+        }
+        MarkerOptions options = new MarkerOptions();
+        options.position(new LatLng(lat,lng));
+        options.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        options.anchor(0.5f,1);
+        options.title(title);
+        options.snippet("snippet - " + title);
+        options.draggable(true);
+
+        marker = map.addMarker(options);
     }
 
     @Override
@@ -122,4 +192,6 @@ public class GoogleMapActivity extends AppCompatActivity
         LatLng target = position.target;
         Projection projection = map.getProjection();
     }
+
+
 }
